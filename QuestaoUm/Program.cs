d8894ae16516e77388be53df6b05c7b634da3164
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2024-12-12 11:13:10 -0300

    Add: Question 1

---
+++ QuestaoUm/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        /* Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada com o nome dele é exibida:  
+         *  Olá, Welisson! Seja muito bem-vindo!
+        */
+
+        Console.WriteLine("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+    }
+}
